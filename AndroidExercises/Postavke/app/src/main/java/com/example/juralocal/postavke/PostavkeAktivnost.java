f34ce961db027dac41ac102c5098d315dad22fc1
SuperJura <jura1994jk@hotmail.com>
2016-08-15 22:08:55 +0200

    new version of android exercise

---
+++ AndroidExercises/Postavke/app/src/main/java/com/example/juralocal/postavke/PostavkeAktivnost.java
@@ -0,0 +1,16 @@
+package com.example.juralocal.postavke;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JuraLocal on 9.6.2016..
+ */
+public class PostavkeAktivnost extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postavke);
+    }
+}
